fbdb6d4e12299303f2ea0fb1f59732b8675aa968
Bill Rayburn <brayburn@infoblox.com>
2022-02-11 00:25:21 +0000

    Remove bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/infoblox-brayburn/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "guarav"
   acl = "private"
   bucket_prefix = "infoblox-brayburn"
 }
